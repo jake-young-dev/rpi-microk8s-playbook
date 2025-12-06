16c8719a8b87c446b427b4c76ba5bdbba31e767f
jake-young-dev <17075259+jake-young-dev@users.noreply.github.com>
2025-12-06 01:23:40 -0500

    adding username and password with groups

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
 read -p "Enter IP Address: " ip_address
-read -p "Enter SSH Username: " ssh_user
-
-ansible-playbook -i $ip_address, ./playbook.yaml -u $ssh_user -K -k
+ansible-playbook -i $ip_address, ./playbook.yaml
