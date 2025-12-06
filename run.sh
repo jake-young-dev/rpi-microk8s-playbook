e87355ac4a13a3185b8860033d3ebaaac86565fd
jake <17075259+jake-young-dev@users.noreply.github.com>
2025-12-06 01:07:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 read -p "Enter IP Address: " ip_address
 read -p "Enter SSH Username: " ssh_user
 
-ansible-playbook -i $ip_address, ./microk8.yaml -u $ssh_user -K -k
+ansible-playbook -i $ip_address, ./playbook.yaml -u $ssh_user -K -k
